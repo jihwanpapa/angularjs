56f81ddc61ab72dd979ecf3b9a2b0d9b564b615e
pollab@naver.com <pollab@naver.com>
2020-08-23 11:43:20 +0900

    initail

---
+++ todo/app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myToDoApp
+ * @description
+ * # myToDoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myToDoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
